272d5fe474e33fb8b918ff4625c449c37813bf8e
Yissel <yisselpereirar@gmail.com>
2022-07-01 09:32:12 -0300

    Sexto commit -  se agrego colision de la bola contra las barras

--- PingPong.js
+++ PingPong.js
@@ -8,7 +8,7 @@ class Board {
     this.ball = null;
     }
         getElements(){
-            var elements = this.bars;
+            var elements = this.bars.map(bar => bar);
             elements.push(this.ball);
             return elements
         }
@@ -20,14 +20,35 @@ class Ball{
         this.radius = radius;
         this.board = board;
         this.speed_y = 0;
-        this.speed_x = 1;
+        this.speed_x = 3;
         this.board.ball = this;
         this.kind = "circle";
         this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
     }
     move(){
         this.x += this.speed_x * this.direction;
         this.y += this.speed_y;
+    }
+    get width(){
+        return this.radius * 2;
+    }
+    get height(){
+        return this.radius * 2;
+    }
+    collisions(bar){
+        //Reacciona a la colision con una barra que recibe como parametro
+        var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+        var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+        this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+        this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+        this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+        if(this.x > (this.board.width / 2)) this.direction = -1;
+        else this.direction = 1;
+    
     }
 }
 class Boardview{
@@ -50,9 +71,41 @@ class Boardview{
         if(this.board.playing){
         this.clean();
         this.draw();
+        this.check_collisions();
         this.board.ball.move();
         }
     }
+    check_collisions(){
+        this.board.bars.forEach(bar=>{
+            if(hit(bar, this.board.ball)){
+                this.board.ball.collisions(bar);
+            }
+        })
+    }
+}
+function hit(a,b){
+    // Revisa si a colisiona con b
+    var hit = false;
+    //Colisiones horizontales
+    if(b.x + b.width >= a.x && b.x < a.x + a.width){
+        //Colisiones verticales
+        if(b.y + b.height >= a.y && b.y < a.y + a.height){
+            hit = true;
+        }
+    }
+    //Colision de a con b
+    if(b.x <= a.x && b.x + b.width >= a.x + a.width){
+        if(b.y <= a.y && b.y + b.height >= a.y + a.height){
+            hit = true;
+        }
+    }
+    //Colision de b con a
+    if(a.x <= b.x && a.x + a.width >= b.x + b.width){
+        if(a.y <= b.y && a.y + a.height >= b.y + b.height){
+            hit = true;
+        }
+    }
+    return hit;
 }
 function draw(ctx,element){
 
