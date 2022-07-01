731affc7bbf7584b439f2add646848715944953d
Yissel <yisselpereirar@gmail.com>
2022-07-01 06:02:40 -0300

    Segundo commit - Se agrego barras y funcion para dibujar los elemtnos

--- PingPong.js
+++ PingPong.js
@@ -9,7 +9,7 @@ class Board {
     }
         getElements(){
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements
         }
 }
@@ -21,6 +21,38 @@ class Boardview{
         this.board = board;
         this.ctx = canvas.getContext("2d");
     }
+    draw(){
+        this.board.getElements().forEach(element => {
+            draw(this.ctx,element)
+        });
+    }
+}
+function draw(ctx,element){
+    if(element!=null){
+        switch(element.kind){
+            case "rectangle":
+                ctx.fillRect(element.x,element.y,element.width,element.height);
+                break;
+        }
+    }
+}
+class Bar {
+    constructor(x, y, width, height, board){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+
+    }
+    down(){
+
+    }
+    up(){
+
+    }
 }
 window.addEventListener("load", main)
 function main(){
@@ -28,4 +60,7 @@ function main(){
     console.log(board);
     var canvas = document.getElementById('canvas')
     var board_view = new Boardview(canvas,board);
+    var bar = new Bar(10,100,40,100,board);
+    var bar2 = new Bar(750,100,40,100,board);
+    board_view.draw();
 }
